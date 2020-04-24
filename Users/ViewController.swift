6e65763185b0521efa93bf437aeafe305d004df1
Arturo bm <arturo.bravo@axity.com>
2020-04-24 17:17:31 -0500

    Initial Commit

---
+++ Users/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Users
+//
+//  Created by Axity on 24/04/20.
+//  Copyright © 2020 Axity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
